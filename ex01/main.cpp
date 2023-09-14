6f53bcccde7f98defb2e750fd2e7b74eb4998d7b
clballes <claramballester@gmail.com>
2023-09-14 21:55:11 +0200

    first stepts

---
+++ ex01/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "lib_phone.h"
+
+int main()
+{
+    PhoneBook phoneBook;
+    phoneBook.Sample();
+    std::string userInput;
+    while (1) {
+        // Prompt the user for input
+        std::cout << "Enter 'ADD' to add a contact, 'SEARCH' to search, or 'EXIT' to exit: ";
+        // Read user input as a string
+        std::cin >> userInput;
+
+        // Check the user's choice and perform the corresponding action
+        if (userInput == "ADD")
+        {
+            std::cout << "You chose to add an item." << std::endl;
+            // Add your add item logic here
+        }
+        else if (userInput == "SEARCH")
+        {
+            std::cout << "You chose to search." << std::endl;
+            // Add your search logic here
+        }
+        else if (userInput == "EXIT") {
+            std::cout << "Exiting the program." << std::endl;
+            break;  // Exit the loop and terminate the program
+        }
+        else {
+            std::cout << "Invalid choice. Please enter 'ADD', 'SEARCH', or 'EXIT'." << std::endl;
+        }
+    }
+
+    return 0;
+}
---
+++ ex01/phone.cpp
@@ -0,0 +1,8 @@
+#include "lib_phone.h"
+#include <iostream>
+
+void PhoneBook::Sample()
+{
+    std::cout << "CLARA BITXS" << std::endl;
+    // Implement your Sample function here
+}
