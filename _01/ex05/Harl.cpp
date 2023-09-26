032ca5eb7d36e5dc8ed61d8081e624a65a79fcd3
clballes <claramballester@gmail.com>
2023-09-26 11:22:54 +0200

    ex05 done

--- _01/ex05/Harl.cpp
+++ _01/ex05/Harl.cpp
@@ -1,42 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clballes <clballes@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 10:39:19 by clballes          #+#    #+#             */
+/*   Updated: 2023/09/26 10:39:20 by clballes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 
-void Harl::info()
+const std::string Harl::str[4] = {
+	"DEBUG",
+	"INFO",
+	"WARNING",
+	"ERROR"
+};
+
+void (Harl::*Harl::f[4])(void) = {
+	&Harl::_debug,
+	&Harl::_info,
+	&Harl::_warning,
+	&Harl::_error
+};
+
+void Harl::_info()
 {
     std::cout << "this is info message" << std::endl;
 }
 
-void Harl::warning()
+void Harl::_warning()
 {
     std::cout << "this is warning message" << std::endl;
 }
 
-void Harl::error()
+void Harl::_error()
 {
     std::cout << "this is error message" << std::endl;
 }
 
-void Harl::debug()
+void Harl::_debug()
 {
     std::cout << "this is debug message" << std::endl;
 }
 
 void Harl::complain( std::string level )
 {
-    std::map<std::string, MemberFunction>::iterator it = levelToFunction.find(level);
-    if (it != levelToFunction.end())
-    {
-        MemberFunction func = it->second;
-        (this->*func)();
-    } 
-    else {
-        std::cout << "Unknown complaint level: " << level << std::endl;
-    }
+	for(int i = 0; i < 4; i++)
+	{
+		if (level  == (this->str[i]))
+		{
+			(this->*f[i])();
+			return ;
+		}
+	}
+	std::cout << "unknown level: " << level << std::endl;
 }
-Harl::Harl() {
-        levelToFunction["debug"] = &Harl::debug;
-        levelToFunction["info"] = &Harl::info;
-        levelToFunction["warning"] = &Harl::warning;
-        levelToFunction["error"] = &Harl::error;
-    }
 
-// Harl::Harl() destructor
+Harl::Harl()
+{
+       return ;
+}
+
+Harl::~Harl()
+{
+    std::cout << "destructor called" << std::endl;
+	return ;
+}
--- _01/ex05/Harl.hpp
+++ _01/ex05/Harl.hpp
@@ -1,21 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clballes <clballes@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 10:39:30 by clballes          #+#    #+#             */
+/*   Updated: 2023/09/26 10:39:31 by clballes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
-#include <map> // Include the map header
 
 #ifndef __HARL_H__
 #define __HARL_H__
 
 class Harl {
     private:
-        void debug( void );
-        void info( void );
-        void warning( void );
-        void error( void);
-        typedef void (Harl::*MemberFunction)();
-        std::map<std::string, MemberFunction> levelToFunction;
+        void _debug( void );
+        void _info( void );
+        void _warning( void );
+        void _error( void);
+		static void (Harl::*f[4])(void);
+		static const std::string str[4];
     public:
-    // Map complaint levels to member function pointers
         Harl();
         void complain( std::string level );
+        ~Harl();
 };
 #endif
