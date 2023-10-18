f7405e81e2b3e18286dea5a19a5e8f2daecd504a
clballes <claramballester@gmail.com>
2023-10-18 20:17:27 +0200

    fet7

--- _07/ex02/Array.hpp
+++ _07/ex02/Array.hpp
@@ -16,38 +16,54 @@ class Array {
 		}
 		Array(unsigned int n)
 		{
-			std::cout << "Default constructor unsigned int array" << n << std::endl;
-			this->_arr = new T();
+			std::cout << "Default constructor unsigned int array len: " << n << std::endl;
+			this->_arr = new T[n];
 			this->_len = n;
 		}
 		Array(Array const & src)
 		{
+			std::cout << "copy constructor" << std::endl;
 			*this = src;
 		}
 		Array & operator=(Array const & rhs)
 		{
 			if (this != &rhs)
 			{
+				std::cout << "operator overload constructor" << std::endl;
 				delete [] this->_arr;
-				_arr = new T[rhs.getSize()];
-				_len = rhs._len;
+				this->_arr = new T[rhs.getSize()];				
+				this->_len = rhs._len;
 			}
-			for ( unsigned int i( 0 ); i < _len; i++ )
-                _arr[i] = rhs._arr[i];
+			for ( unsigned int i = 0; i < this->_len; i++ )
+                this->_arr[i] = rhs._arr[i];
         	return *this;
 		}
+		T& operator[](unsigned int index) const
+		{
+			if (index >= _len || index < 0) 
+    		{
+				throw OutOfBoundsException();
+			}
+				return _arr[index];
+    	}
 		~Array()
 		{
 			delete [] _arr;
 			std::cout << "Default destructor array" << std::endl;
 		}
-		void getSize( void )
+		size_t getSize( void ) const
 		{
 			return this->_len;
 		}
+
+		class OutOfBoundsException : public std::exception {
+        public:
+            virtual const char* what() const throw() { return "Index is out of bounds";}
+    };
+
 	private:
-		T *		_arr;
-		size_t	_len;
+		T *				_arr;
+		unsigned int	_len;
 };
 
 #endif
--- _07/ex02/main.cpp
+++ _07/ex02/main.cpp
@@ -3,12 +3,47 @@
 
 int main( void )
 {
-	Array<int>           ai;
-	int * a = new int();
-	std::cout << *a << std::endl;
-	// Array<float>         af;
-	// Array<char*>         ac;
-	// Array<std::string>   as;
-	// Array<Array<int> >    aai;
+	Array<int> originalArray(5);
+    for (int i = 0; i < 5; ++i) {
+        originalArray[i] = i * 10;
+    }
+
+    // Use the copy constructor to create a new array with the same content
+    Array<int> copiedArray(originalArray);
+
+    // Use the assignment operator to copy the content of one array to another
+    Array<int> assignedArray;
+    assignedArray = originalArray;
+
+    // Display the content of the arrays
+    std::cout << "Original Array: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << originalArray[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Copied Array: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << copiedArray[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Assigned Array: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << assignedArray[i] << " ";
+    }
+    std::cout << std::endl;
+	// Array<int> intArray(2); // Instantiate an Array of integers with a size of 2
+	//  try {
+    //     intArray[0] = 42;
+    //     intArray[1] = 73;
+    //     intArray[2] = 99;
+    // } catch ( Array< int >::OutOfBoundsException& e ) {
+    //     std::cerr << "Exception: " << e.what() << std::endl;
+    // }
+	// //try copy array overlaod
+	// Array<int> intArrcopy(intArray);
+
+	// std::cout << "hey "<< intArrcopy.getSize() << std::endl;
     return 0;
 }
